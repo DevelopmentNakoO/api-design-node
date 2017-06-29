e19595b25f42402e623a8c5fcc38bd92fac8f34e
Olesya <olesya.nakonechnaya@verizon.com>
2017-06-29 10:03:49 -0500

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Olesya Nakonechnaya
+// Allie Smith 
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
