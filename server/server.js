2b08800a790fbee2a53aabec273f13b9c745306d
Olesya <olesya.nakonechnaya@verizon.com>
2017-07-09 14:05:31 -0500

    step2

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(express.static('client'));
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(bodyParser.json());
 
 
@@ -22,6 +23,42 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req,res){
+	res.json(lions);
+});
+
+app.get('/lions/:id', function(req, res){
+	var lion = _.find(lions, {id: req.params.id});
+	res.json(lion || {});
+});
+
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+
+  lions.push(lion);
+
+  res.json(lion);
+});
+
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+
 
 app.listen(3000);
 console.log('on port 3000');
